2b794c5789c6e2f02f3ea36802e9087b0bb73f53
Richard Sweeney <theorobman@gmail.com>
2013-03-29 20:08:29 +0100

    Convert spaces to tabs

--- js/admin.js
+++ js/admin.js
@@ -1,53 +1,66 @@
 jQuery( function ( $ ) {
 	"use strict";
 
-    var
-        $new                = $( '.new' ),
-        $newTrigger         = $( '.new-trigger a' ),
-        $concertpressSelect = $( '.concertpress-select' ),
-        $multiDateTrigger   = $( '.concertpress-multi-date-trigger' ),
-        $endDate            = $( '.end-date' ),
-        $hourMin            = $( '.concertpress-time')
-
-    if ( 'event' === pagenow )
-        $new.hide()
-
-    if ( !$multiDateTrigger.is( ':checked' ) )
-        $endDate.hide()
-
-    $newTrigger.on( 'click', function ( e ) {
-
-        e.preventDefault()
-        $( this ).parent().hide().next().slideDown( 350 )
-        $( this ).parent().prev().val( 0 )
-
-    })
-
-    $concertpressSelect.on( 'change', function( e ) {
-        var $trigger = $( this ).next()
-
-        if ( !$trigger.is( ':visible' ) )
-            $trigger.show().next().slideUp( 200 )
-
-    })
+	var
+		$new                = $( '.new' ),
+		$newTrigger         = $( '.new-trigger a' ),
+		$concertpressSelect = $( '.concertpress-select' ),
+		$multiDateTrigger   = $( '.concertpress-multi-date-trigger' ),
+		$endDate            = $( '.end-date' ),
+		$startDate          = $( '.start-date' ),
+		$hourMin            = $( '.concertpress-time');
 
 
-    $.each( $( '.concertpress-datepicker' ), function () {
-        $( this ).datepicker({
-            regional  : [ cp.language ],
-            dateFormat: 'yy-mm-dd',
-            firstDay  : 1
-        })
-    })
+	if ( 'event' === pagenow )
+		$new.hide();
 
-    $multiDateTrigger.on( 'change', function () {
-        if ( !$endDate.is( ':visible' ) ) {
-            $endDate.slideDown( 200 )
-            $hourMin.attr( 'disabled', true )
-        } else {
-            $endDate.slideUp( 200 )
-            $hourMin.attr( 'disabled', false )
-        }
-    })
+	if ( !$multiDateTrigger.is( ':checked' ) )
+		$endDate.hide();
+
+
+	$newTrigger.on( 'click', function ( e ) {
+
+		e.preventDefault();
+		$( this ).parent().hide().next().slideDown( 350 );
+		$( this ).parent().prev().val( 0 );
+
+	});
+
+
+	$concertpressSelect.on( 'change', function( e ) {
+		var $trigger = $( this ).next();
+
+		if ( ! $trigger.is( ':visible' ) )
+			$trigger.show().next().slideUp( 200 );
+
+	});
+
+
+	/** Datepicker */
+	$.each( $( '.concertpress-datepicker' ), function () {
+		$( this ).datepicker({
+			monthNames: $.parseJSON( cp.monthNames ),
+			monthNamesShort: $.parseJSON( cp.monthNamesShort ),
+			dayNames: $.parseJSON( cp.dayNames ),
+			dayNamesShort: $.parseJSON( cp.dayNamesShort ),
+			dateFormat: 'yy-mm-dd',
+			firstDay  : 1
+		});
+	});
+
+
+	/** Show the end date */
+	$multiDateTrigger.on( 'change', function () {
+
+		if ( !$endDate.is( ':visible' ) ) {
+			$endDate.slideDown( 200 );
+			$hourMin.attr( 'disabled', true );
+		} else {
+			$endDate.slideUp( 200 );
+			$hourMin.attr( 'disabled', false );
+		}
+
+	});
+
 
 });
